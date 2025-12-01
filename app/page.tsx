6c2d6a4117ce89ee5100d2c0ab4dfbfa3f24eacd
DalipTandon <daliptandon8@gmail.com>
2025-12-01 23:49:46 +0530

    Add:reactbit lightray component

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,12 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <h1>
-      welcome to nextjs 13.4 with turbopack!
+    <section>
+    <h1 className="text-center">
+    The Hub for every Developer <br/> Event Worldwide that you can't miss!
     </h1>
+    <p className="text-center mt-5">Hackathons,Meetups,Conference. All at one Place</p>
+    </section>
+   
   );
 }
