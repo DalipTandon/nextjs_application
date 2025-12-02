537fd283d4ed3341c68542fb7a96fb8b40c0e791
DalipTandon <daliptandon8@gmail.com>
2025-12-02 20:13:28 +0530

    Add:Navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import ExploreBtn from "../components/ExploreBtn";
 import Image from "next/image";
 
 export default function Home() {
@@ -7,6 +8,17 @@ export default function Home() {
     The Hub for every Developer <br/> Event Worldwide that you can't miss!
     </h1>
     <p className="text-center mt-5">Hackathons,Meetups,Conference. All at one Place</p>
+    <ExploreBtn />
+    <div className="mt-20 space-y-7">
+    <h3>Featured events</h3>
+    <ul>
+      {[1,2,3,4,5].map((event)=>(
+        <li key={event}>
+          Event {event} 
+        </li>
+      ))}
+    </ul>
+    </div>
     </section>
    
   );
