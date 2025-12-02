6dab8d7984db56e346cc6ed486b6994153d9750c
DalipTandon <daliptandon8@gmail.com>
2025-12-02 20:56:52 +0530

    Add:Eventcard to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import EventCard from "@/components/EventCard";
 import ExploreBtn from "../components/ExploreBtn";
 import Image from "next/image";
+import events from "@/lib/constants";
 
-export default function Home() {
+export default function Page() {
   return (
     <section>
     <h1 className="text-center">
@@ -11,10 +13,10 @@ export default function Home() {
     <ExploreBtn />
     <div className="mt-20 space-y-7">
     <h3>Featured events</h3>
-    <ul>
-      {[1,2,3,4,5].map((event)=>(
-        <li key={event}>
-          Event {event} 
+    <ul className="events">
+      {events.map((event)=>(
+        <li key={event.title}>
+          <EventCard {...event}/>
         </li>
       ))}
     </ul>
